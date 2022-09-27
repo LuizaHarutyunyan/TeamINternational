f38965a7bf27e10768c4507b78c10318c913dece
luiza <louisa.harutyunyan.98@Gmail.com>
2022-09-27 21:14:17 +0200

    first commit

---
+++ palindrome.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace app
+{
+    class palindrome
+    {
+        static void Main(string[] args){
+
+         Console.WriteLine("Enter your input");
+         string palindromeInput=Console.ReadLine();
+         String result = palindromeCheck(palindromeInput);
+         Console.WriteLine(result);
+         }
+
+        static string palindromeCheck(string palindromeInput){
+            string input=palindromeInput.ToUpper();// convert to uppercase
+            
+            int length=input.Length;
+            for (int i=0;i<length/2;i++){
+                if (input[i]!=input[length-i-1])
+                   return "Is not palindrome"
+            
+            
+            }
+            return "Is palindrome";
+        }
+    }
+
+         
+
+
+}
